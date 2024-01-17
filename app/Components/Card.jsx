a8f48bb69446f0df8bc3f9cbb65b4a6219130fb7
arjunshetty1 <arjunjayapura@mail.com>
2024-01-18 02:11:23 +0530

    update

--- app/Components/Card.jsx
+++ app/Components/Card.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import DatePickerUi from "./DatePickerUi";
+import { createOrder } from "../Services/apiProducts";
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const price = e.target.price.value;
-  // const id = e.target.form.id.value;
-
-  console.log("Neha I Love You", price);
-};
 
 const Card = ({ item }) => {
+  const [send, setsend] = useState({
+    productName: item.productName,
+    productPrice: item.price,
+    productId: item.id,
+  });
+
+
+  const handleSubmit = async () => {
+    console.log(send);
+    const response = await createOrder(send);
+    console.log(response);
+  };
+
+  
+
   return (
     <>
-      <div className="card w-72  bg-base-100 shadow-xl transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:text-black  hover:bg-slate-50 hover:outline hover:outline-3 hover:outline-offset-2 hover:duration-200">
+      <div
+        className="card w-72  bg-base-100 shadow-xl transition ease-in-out delay-150 
+       hover:-translate-y-1 hover:scale-110 hover:text-black  hover:bg-slate-50 hover:outline 
+       hover:outline-3 hover:outline-offset-2 hover:duration-200"
+      >
         <figure>
           <img
             src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
@@ -29,66 +43,56 @@ const Card = ({ item }) => {
             >
               Rent Now
             </button>
-            <form action="#" onSubmit={handleSubmit}>
-              <dialog
-                key={item.id}
-                id={item.id}
-                name="id"
-                className="modal text-white"
-              >
-                <div className="modal-box md:min-w-[1200px] md:min-h-[460px] p-10">
-                  <div className="flex gap-14 mt-12 ">
-                    <div className="left w-1/2">
-                      <img
-                        className="rounded-lg "
-                        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="right flex flex-col gap-3 w-1/2">
-                      <p
-                        className="uppercase text-xl font-medium"
-                        data-productname={item.productName}
+
+            <dialog key={item.id} id={item.id} className="modal text-white">
+              <div className="modal-box md:min-w-[1200px] md:min-h-[460px] p-10">
+                <div className="flex gap-14 mt-12 ">
+                  <div className="left w-1/2">
+                    <img
+                      className="rounded-lg "
+                      src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="right flex flex-col gap-3 w-1/2">
+                    <p className="uppercase text-xl font-medium">
+                      {item.productName}
+                    </p>
+                    <DatePickerUi />
+                    <p className="text-xl relative top-5 font-normal" n>
+                      Total Cost {item.price}
+                    </p>
+                    <button
+                      type="button"
+                      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                       focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex
+                        items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={handleSubmit}
+                    >
+                      Proceed
+                      <svg
+                        class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
                       >
-                        {item.productName}
-                      </p>
-                      <DatePickerUi />
-                      <p
-                        className="text-xl relative top-5 font-normal"
-                        name="Price"
-                      >
-                        Total Cost {item.price}
-                      </p>
-                      <button
-                        type="button"
-                        onClick={handleSubmit}
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                        Proceed
-                        <svg
-                          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                      </button>
-                    </div>
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                      </svg>
+                    </button>
                   </div>
                 </div>
-                <form method="dialog" className="modal-backdrop">
-                  <button>close</button>
-                </form>
-              </dialog>
-            </form>
+              </div>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
+            </dialog>
           </div>
         </div>
       </div>
--- app/Services/apiProducts.js
+++ app/Services/apiProducts.js
@@ -21,3 +21,33 @@ export const product = async () => {
   );
   return response;
 };
+
+export const createOrder = async (send) => {
+  const mutationQuery =
+    gql`
+    mutation MyMutation {
+      createOrderDetail(
+        data: {productName: "` +
+    send.productName +
+    `", productPrice: ${send.productPrice}, productId: "` +
+    send.productId +
+    `", product: {connect: {id: "` +
+    send.productId +
+    `"}}}
+      ) {
+
+        id
+        
+        }
+    }
+  `;
+  const response = await request(
+    "https://api-ap-south-1.hygraph.com/v2/clr3vfiglcadr01waj5mgj7xh/master",
+    mutationQuery
+  );
+
+  if (response.errors) {
+    console.log(response.errors);
+  }
+  return response;
+};
