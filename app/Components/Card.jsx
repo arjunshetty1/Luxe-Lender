7d7db4b72b3dea9b45c6a970a05d42eb08b449b5
arjunshetty1 <arjunjayapura@mail.com>
2024-03-20 16:20:46 +0530

    Modal Close button added

--- app/Components/Card.jsx
+++ app/Components/Card.jsx
@@ -68,7 +68,10 @@ const Card = ({ item }) => {
                     onClick={() => document.getElementById(item.id).close()}
                     className="absolute top-4 right-6 text-lg md:text-2xl cursor-pointer text-black"
                   >
-                    X
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
                   </button>
                   <div className="md:flex md:flex-row flex-col gap-14 mt-12 ">
                     <div>
@@ -109,6 +112,9 @@ const Card = ({ item }) => {
                           addToCart();
                         }}
                       >
+
+
+                        
                         Add to Cart
                         <svg
                           class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
