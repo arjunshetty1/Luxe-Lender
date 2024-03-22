e0e1d8b93e07bc881f1ea10c5ca398a94e58b849
Arjun Shetty <97979919+arjunshetty1@users.noreply.github.com>
2024-03-22 10:00:02 +0530

    Update ProductsPage.jsx

--- app/Components/ProductsPage.jsx
+++ app/Components/ProductsPage.jsx
@@ -9,12 +9,7 @@ const ProductsPage = ({ Prddata, isloading }) => {
         <div>
           <section className="bg-white ">
             <div className="container px-6 py-10 mx-auto animate-pulse">
-              {/* <h1 className="w-48 h-2 mx-auto bg-gray-200 rounded-lg dark:bg-gray-700"></h1> */}
-
-              {/* <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-              <p className="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80 dark:bg-gray-700"></p> */}
-
-              <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+              <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-4">
                 <div className="w-full ">
                   <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
 
@@ -77,10 +72,14 @@ const ProductsPage = ({ Prddata, isloading }) => {
       ) : (
         ""
       )}
-      <div className="grid grid-cols-2 mt-4 md:grid-cols-2 lg:grid-cols-3  gap-0 sm:gap-8">
-        {Prddata?.products?.map((item) => {
-          return <Card item={item} key={item.id} />;
-        })}
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8">
+            {Prddata?.products?.map((item) => {
+              return <Card item={item} key={item.id} />;
+            })}
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
